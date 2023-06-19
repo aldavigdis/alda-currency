3ef058cb7a7a9fcd1fc64574683eb4af1adc52d0
Alda Vigdís Skarphéðinsdóttir <aldavigdis@aldavigdis.is>
2023-06-19 21:51:35 +0200

    Linting the CurrencyConverter component

--- src/currencyConverter.js
+++ src/currencyConverter.js
@@ -1,17 +1,16 @@
-
 class CurrencyConverter extends React.Component {
-	constructor(props) {
-		super(props);
+	constructor( props ) {
+		super( props );
 		this.state = {
 			baseCurrency: '',
 			initialBaseCurrencyAmount: 1,
-			baseCurrencyAmount: (1).toFixed(2),
+			baseCurrencyAmount: ( 1 ).toFixed( 2 ),
 			currencyRates: {},
-			currencyAmounts: {}
+			currencyAmounts: {},
 		};
 
-		this.onUpdatePrimaryAmount = this.onUpdatePrimaryAmount.bind(this);
-		this.onUpdateCurrencyAmount = this.onUpdateCurrencyAmount.bind(this);
+		this.onUpdatePrimaryAmount = this.onUpdatePrimaryAmount.bind( this );
+		this.onUpdateCurrencyAmount = this.onUpdateCurrencyAmount.bind( this );
 	}
 
 	componentDidMount() {
@@ -19,45 +18,52 @@ class CurrencyConverter extends React.Component {
 	}
 
 	async getInfo() {
-		const response = await fetch( wpApiSettings.root + 'alda-currency/v1/info' );
+		const response = await fetch(
+			wpApiSettings.root + 'alda-currency/v1/info'
+		);
 		const result = await response.json();
 
-		this.setState({
+		this.setState( {
 			baseCurrency: result.base_currency,
 			currencyRates: result.rates,
-			currencyAmounts: result.rates
-		});
+			currencyAmounts: result.rates,
+		} );
 
 		return result;
 	}
 
-	onUpdatePrimaryAmount(event) {
+	onUpdatePrimaryAmount( event ) {
 		const newCurrencyAmounts = {};
-		for (const key in this.state.currencyAmounts) {
-			newCurrencyAmounts[key] = this.state.currencyRates[key] * event.target.value;
+		for ( const key in this.state.currencyAmounts ) {
+			newCurrencyAmounts[ key ] =
+				this.state.currencyRates[ key ] * event.target.value;
 		}
-		this.setState(
-			{
-				baseCurrencyAmount: parseFloat(event.target.value).toFixed(2),
-				currencyAmounts: newCurrencyAmounts
-			}
-		);
+		this.setState( {
+			baseCurrencyAmount: parseFloat( event.target.value ).toFixed( 2 ),
+			currencyAmounts: newCurrencyAmounts,
+		} );
 	}
 
-	onUpdateCurrencyAmount(event) {
+	onUpdateCurrencyAmount( event ) {
 		const currency = event.target.name;
-		const newBaseCurrencyAmount = ( this.state.initialBaseCurrencyAmount / parseFloat(this.state.currencyRates[currency]) * parseFloat(event.target.value) ).toFixed(2);
+		const newBaseCurrencyAmount = (
+			( this.state.initialBaseCurrencyAmount /
+				parseFloat( this.state.currencyRates[ currency ] ) ) *
+			parseFloat( event.target.value )
+		).toFixed( 2 );
 		const newCurrencyAmounts = {};
-		newCurrencyAmounts[currency] = parseFloat(event.target.value);
-		for (const key in this.state.currencyAmounts) {
-			if ( key != currency ) {
-				newCurrencyAmounts[key] = (parseFloat(this.state.currencyRates[key]) * parseFloat(newBaseCurrencyAmount));
+		newCurrencyAmounts[ currency ] = parseFloat( event.target.value );
+		for ( const key in this.state.currencyAmounts ) {
+			if ( key !== currency ) {
+				newCurrencyAmounts[ key ] =
+					parseFloat( this.state.currencyRates[ key ] ) *
+					parseFloat( newBaseCurrencyAmount );
 			}
 		}
-		this.setState({
+		this.setState( {
 			baseCurrencyAmount: newBaseCurrencyAmount,
-			currencyAmounts: newCurrencyAmounts
-		});
+			currencyAmounts: newCurrencyAmounts,
+		} );
 	}
 
 	render() {
@@ -66,42 +72,47 @@ class CurrencyConverter extends React.Component {
 				<tbody>
 					<tr>
 						<th
-							scope='row'
+							scope="row"
 							data-currency={ this.state.baseCurrency }
 						>
 							{ this.state.baseCurrency }
 						</th>
-						<td>{ (this.state.initialBaseCurrencyAmount).toFixed(2) }</td>
+						<td>
+							{ this.state.initialBaseCurrencyAmount.toFixed(
+								2
+							) }
+						</td>
 						<td>
 							<input
-								type='number'
-								step='0.01'
+								type="number"
+								step="0.01"
 								name={ this.state.baseCurrency }
 								value={ this.state.baseCurrencyAmount }
 								onChange={ this.onUpdatePrimaryAmount }
 							/>
 						</td>
 					</tr>
-					{Object.keys(this.state.currencyRates).filter(k => k != this.state.baseCurrency).map(key => (
-						<tr key={ key }>
-							<th
-								scope='row'
-								data-currency={ key }
-							>
-								{ key }
-							</th>
-							<td>{ this.state.currencyRates[key] }</td>
-							<td>
-								<input
-									type='number'
-									step='0.01'
-									name={ key }
-									value={ ( this.state.currencyAmounts[key] ).toFixed(2) }
-									onChange={ this.onUpdateCurrencyAmount }
-								/>
-							</td>
-						</tr>
-					))}
+					{ Object.keys( this.state.currencyRates )
+						.filter( ( k ) => k !== this.state.baseCurrency )
+						.map( ( key ) => (
+							<tr key={ key }>
+								<th scope="row" data-currency={ key }>
+									{ key }
+								</th>
+								<td>{ this.state.currencyRates[ key ] }</td>
+								<td>
+									<input
+										type="number"
+										step="0.01"
+										name={ key }
+										value={ this.state.currencyAmounts[
+											key
+										].toFixed( 2 ) }
+										onChange={ this.onUpdateCurrencyAmount }
+									/>
+								</td>
+							</tr>
+						) ) }
 				</tbody>
 			</table>
 		);
