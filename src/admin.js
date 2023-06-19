27b6d80f5f710c00655c0f16d611e7a53afef8cb
Alda Vigdís Skarphéðinsdóttir <aldavigdis@aldavigdis.is>
2023-06-19 21:45:47 +0200

    Linting the admin JS

--- src/admin.js
+++ src/admin.js
@@ -1,40 +1,40 @@
 class AldaCurrencyUI {
 	static ratesTab() {
-		return document.querySelector('#nav-tab-rates');
+		return document.querySelector( '#nav-tab-rates' );
 	}
 
 	static settingsTab() {
-		return document.querySelector('#nav-tab-settings');
+		return document.querySelector( '#nav-tab-settings' );
 	}
 
 	static ratesForm() {
-		return document.querySelector('#alda-currency-rates-form')
+		return document.querySelector( '#alda-currency-rates-form' );
 	}
 
 	static settingsForm() {
-		return document.querySelector('#alda-currency-settings-form')
+		return document.querySelector( '#alda-currency-settings-form' );
 	}
 
-	static settingsTabClickEvent(event) {
+	static settingsTabClickEvent( event ) {
 		event.preventDefault();
-		AldaCurrencyUI.ratesForm().classList.add('hidden');
-		AldaCurrencyUI.settingsForm().classList.remove('hidden');
-		AldaCurrencyUI.ratesTab().classList.remove('nav-tab-active');
-		AldaCurrencyUI.settingsTab().classList.add('nav-tab-active');
+		AldaCurrencyUI.ratesForm().classList.add( 'hidden' );
+		AldaCurrencyUI.settingsForm().classList.remove( 'hidden' );
+		AldaCurrencyUI.ratesTab().classList.remove( 'nav-tab-active' );
+		AldaCurrencyUI.settingsTab().classList.add( 'nav-tab-active' );
 	}
 
-	static ratesTabClickEvent(event) {
+	static ratesTabClickEvent( event ) {
 		event.preventDefault();
-		AldaCurrencyUI.ratesForm().classList.remove('hidden');
-		AldaCurrencyUI.settingsForm().classList.add('hidden');
-		AldaCurrencyUI.ratesTab().classList.add('nav-tab-active');
-		AldaCurrencyUI.settingsTab().classList.remove('nav-tab-active');
+		AldaCurrencyUI.ratesForm().classList.remove( 'hidden' );
+		AldaCurrencyUI.settingsForm().classList.add( 'hidden' );
+		AldaCurrencyUI.ratesTab().classList.add( 'nav-tab-active' );
+		AldaCurrencyUI.settingsTab().classList.remove( 'nav-tab-active' );
 	}
 }
 
 class AldaCurrencySettings {
 	static form() {
-		return document.querySelector('#alda-currency-settings-form');
+		return document.querySelector( '#alda-currency-settings-form' );
 	}
 
 	static submitButton() {
@@ -46,10 +46,10 @@ class AldaCurrencySettings {
 	static baseCurrencySelect() {
 		return document.querySelector(
 			'#alda-currency-settings-form select[name=base_currency]'
-		)
+		);
 	}
 
-	static submitEvent(event) {
+	static submitEvent( event ) {
 		event.preventDefault();
 
 		AldaCurrencySettings.saveBaseCurrency(
@@ -57,37 +57,35 @@ class AldaCurrencySettings {
 		);
 	}
 
-	static saveBaseCurrency( currency_code ) {
+	static saveBaseCurrency( currencyCode ) {
 		AldaCurrencySettings.submitButton().disabled = true;
 
-		let endpointUrl = wpApiSettings.root + 'alda-currency/v1/base-currency';
-		let request = new XMLHttpRequest();
+		const endpointUrl =
+			wpApiSettings.root + 'alda-currency/v1/base-currency';
+		const request = new XMLHttpRequest();
 
 		request.open( 'POST', endpointUrl, true );
 
 		request.setRequestHeader(
-			'Content-Type', 'application/json;charset=UTF-8'
+			'Content-Type',
+			'application/json;charset=UTF-8'
 		);
 
 		request.setRequestHeader( 'X-WP-Nonce', wpApiSettings.nonce );
 
-		request.send(
-			JSON.stringify( { 'currency_code': currency_code } )
-		);
+		request.send( JSON.stringify( { currencyCode } ) );
 
-		request.onreadystatechange = function( request ) {
-			if ( this.status == 200 ) {
+		request.onreadystatechange = function () {
+			if ( this.status === 200 ) {
 				AldaCurrencySettings.submitButton().disabled = false;
 			}
-		}
+		};
 	}
 }
 
 class AldaCurrencyRates {
 	static form() {
-		return document.querySelector(
-			'#alda-currency-rates-form'
-		);
+		return document.querySelector( '#alda-currency-rates-form' );
 	}
 
 	static submitButton() {
@@ -103,87 +101,84 @@ class AldaCurrencyRates {
 	}
 
 	static formRates() {
-		let currencyRates = {};
-		document.querySelectorAll('input.currency-rate').forEach(function(element) {
-			currencyRates[element.name] = parseFloat(element.value);
-		});
+		const currencyRates = {};
+		document
+			.querySelectorAll( 'input.currency-rate' )
+			.forEach( function ( element ) {
+				currencyRates[ element.name ] = parseFloat( element.value );
+			} );
 		return currencyRates;
 	}
 
 	static formEnabledCurrencies() {
-		let enabledCurrencies = [];
-		AldaCurrencyRates.enabledCheckboxes().forEach((checkbox) => {
+		const enabledCurrencies = [];
+		AldaCurrencyRates.enabledCheckboxes().forEach( ( checkbox ) => {
 			if ( checkbox.checked ) {
 				enabledCurrencies.push(
-					checkbox.attributes['data-currency'].value
+					checkbox.attributes[ 'data-currency' ].value
 				);
 			}
-		});
+		} );
 		return enabledCurrencies;
 	}
 
 	static saveCurrencyRates() {
 		AldaCurrencyRates.submitButton().disabled = true;
 
-		let currencyRates = AldaCurrencyRates.formRates();
-		let enabledCurrencies = AldaCurrencyRates.formEnabledCurrencies();
-		let requestBody = {
+		const currencyRates = AldaCurrencyRates.formRates();
+		const enabledCurrencies = AldaCurrencyRates.formEnabledCurrencies();
+		const requestBody = {
 			rates: currencyRates,
-			enabledCurrencies: enabledCurrencies
+			enabledCurrencies,
 		};
 
-		let endpointUrl = wpApiSettings.root + 'alda-currency/v1/rates';
-		let request = new XMLHttpRequest();
+		const endpointUrl = wpApiSettings.root + 'alda-currency/v1/rates';
+		const request = new XMLHttpRequest();
 
 		request.open( 'POST', endpointUrl, true );
 
 		request.setRequestHeader(
-			'Content-Type', 'application/json;charset=UTF-8'
+			'Content-Type',
+			'application/json;charset=UTF-8'
 		);
 
 		request.setRequestHeader( 'X-WP-Nonce', wpApiSettings.nonce );
 
-		request.send(
-			JSON.stringify( requestBody )
-		);
+		request.send( JSON.stringify( requestBody ) );
 
-		request.onreadystatechange = function( request ) {
-			if ( this.status == 200 ) {
+		request.onreadystatechange = function () {
+			if ( this.status === 200 ) {
 				AldaCurrencyRates.submitButton().disabled = false;
 			}
-		}
+		};
 	}
 
-	static submitEvent(event) {
+	static submitEvent( event ) {
 		event.preventDefault();
 		AldaCurrencyRates.saveCurrencyRates();
 	}
 }
 
-window.addEventListener(
-	'DOMContentLoaded',
-	( event ) =>
-	{
-		if ( document.body.classList.contains('toplevel_page_alda-currency') ) {
-			AldaCurrencySettings.form().addEventListener(
-				'submit',
-				AldaCurrencySettings.submitEvent
-			);
+window.addEventListener( 'DOMContentLoaded', () => {
+	if ( document.body.classList.contains( 'toplevel_page_alda-currency' ) ) {
+		AldaCurrencySettings.form().addEventListener(
+			'submit',
+			AldaCurrencySettings.submitEvent
+		);
 
-			AldaCurrencyRates.form().addEventListener(
-				'submit',
-				AldaCurrencyRates.submitEvent
-			);
+		AldaCurrencyRates.form().addEventListener(
+			'submit',
+			AldaCurrencyRates.submitEvent
+		);
 
-			AldaCurrencyUI.settingsTab().addEventListener(
-				'click',
-				AldaCurrencyUI.settingsTabClickEvent
-			);
+		AldaCurrencyUI.settingsTab().addEventListener(
+			'click',
+			AldaCurrencyUI.settingsTabClickEvent
+		);
 
-			AldaCurrencyUI.ratesTab().addEventListener(
-				'click',
-				AldaCurrencyUI.ratesTabClickEvent
-			);
-		}
+		AldaCurrencyUI.ratesTab().addEventListener(
+			'click',
+			AldaCurrencyUI.ratesTabClickEvent
+		);
 	}
-)
+} );
