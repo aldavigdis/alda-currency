016ed5b24e2aea0c8d251ee09fb33cb32d824cad
Alda Vigdís Skarphéðinsdóttir <aldavigdis@aldavigdis.is>
2023-06-19 20:39:57 +0200

    Adding comments to the frontend JS

--- src/frontend.js
+++ src/frontend.js
@@ -1,6 +1,13 @@
+// Import the currency converter React component
 import CurrencyConverter from '../build/currency-converter.js';
-const containers = document.querySelectorAll('.alda-currency-frontend-block-container');
 
+// Find matching WordPress blocks
+const containers = document.querySelectorAll(
+	'.alda-currency-frontend-block-container'
+);
+
+// Assign a root and create a CurrencyConverter component in every DOM element
+// matching the query selector.
 containers.forEach(function (container) {
 	let currency_converter_root = ReactDOM.createRoot(container);
   currency_converter_root.render(React.createElement(CurrencyConverter, null));
